fa1b73279868c1556dc4d9fd1e8df7e618f9f972
RandomLorem <103768218+H4r5h-10@users.noreply.github.com>
2024-01-18 06:47:43 +0530

    array added

--- assets/data/dummydata.js
+++ assets/data/dummydata.js
@@ -2,7 +2,7 @@ import { AiOutlineCalendar } from "react-icons/ai"
 import { TfiRulerPencil } from "react-icons/tfi"
 import { VscFileSubmodule } from "react-icons/vsc"
 import { BiUser } from "react-icons/bi"
-
+export const showcase=[];
 export const home = [
   {
     icon: <AiOutlineCalendar size={25} />,
